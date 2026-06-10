c3daaf6f13a35431808935c598a000ff1669ee91
sahancayirci8 <sahancayirci8@icloud.com>
2026-06-10 21:52:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+let data = JSON.parse(localStorage.getItem("randevu")) || [];
+
+function save(){
+  let r = {
+    name: document.getElementById("name").value,
+    phone: document.getElementById("phone").value,
+    service: document.getElementById("service").value,
+    date: document.getElementById("date").value,
+    time: document.getElementById("time").value
+  };
+
+  if(!r.name || !r.phone){
+    alert("Lütfen tüm alanları doldur");
+    return;
+  }
+
+  data.push(r);
+  localStorage.setItem("randevu", JSON.stringify(data));
+
+  alert("Randevu oluşturuldu 💖");
+}
+
+function login(){
+  let pass = document.getElementById("pass").value;
+
+  if(pass === "1234"){
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    data.forEach(r=>{
+      list.innerHTML += `
+        <li>${r.name} | ${r.phone} | ${r.service} | ${r.date} ${r.time}</li>
+      `;
+    });
+  } else {
+    alert("Hatalı şifre");
+  }
+}
